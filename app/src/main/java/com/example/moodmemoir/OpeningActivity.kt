a58d3b7606974cce13ca94f5c85b319568f196cb
A <A@AGUERRO>
2023-11-21 16:47:41 +0800

    First version

---
+++ app/src/main/java/com/example/moodmemoir/OpeningActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moodmemoir
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OpeningActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.opening_main)
+    }
+}
