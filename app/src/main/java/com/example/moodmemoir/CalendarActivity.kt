b8805ca9fef0e09ad1641b256c24aa64598ccac8
Lance Eugene U. Magtaka <lanceeugene.magtaka.cics@ust.edu.ph>
2023-12-14 21:16:27 +0700

    Account Activity and Calendar (ny completed)

--- app/src/main/java/com/example/moodmemoir/CalendarActivity.kt
+++ app/src/main/java/com/example/moodmemoir/CalendarActivity.kt
@@ -1,12 +1,35 @@
 package com.example.moodmemoir
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Calendar
 
 class CalendarActivity : AppCompatActivity() {
+
+    private lateinit var calendarView: calendarView
+    private val noteCalendarManager = NoteCalendarManager() // Reuse your existing note manager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calendarView = findViewById(R.id.calendarView) // Get reference to CalendarView
+
+        // Set listener for calendar date change
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val selectedDate = Calendar.getInstance().apply {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, month)
+                set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            }
+            val notes = noteCalendarManager.getNotesForDate(selectedDate) // Get notes for selected date
+
+            // Update the UI with retrieved notes (e.g., show them in a list, toast message, etc.)
+
+            // Check for empty notes and display a message if needed
+            if (notes.isEmpty()) {
+                Toast.makeText(this, "No notes found for this date.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
-
